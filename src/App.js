1ff07dab0ba224476aae9313f07db2166636e3f7
fazimfm <fazimwrkid@gmail.com>
2025-11-24 16:13:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,132 @@
+import React, { Component } from "react";
+import "./App.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+  showModal: false,
+  timer: 60, // 60 seconds countdown
+  isRunning: false,
+};
+this.state = {
+  showModal: false,
+  timer: 60,
+  isRunning: false,
+
+  unitInput: "",
+  calculatedBill: null,
+};
+  }
+
+  startTimer = () => {
+  if (!this.state.isRunning) {
+    this.setState({ isRunning: true });
+    this.timerInterval = setInterval(() => {
+      this.setState(prevState => {
+        if (prevState.timer <= 1) {
+          clearInterval(this.timerInterval);
+          toast.info("⏰ Timer finished!", {
+            position: "bottom-center",
+            autoClose: 2000,
+          });
+          return { timer: 0, isRunning: false };
+        }
+        return { timer: prevState.timer - 1 };
+      });
+    }, 1000);
+  }
+};
+
+stopTimer = () => {
+  clearInterval(this.timerInterval);
+  this.setState({ isRunning: false });
+};
+handleKeypad = (value) => {
+  if (value === "C") {
+    this.setState({ unitInput: "", calculatedBill: null });
+  } else {
+    this.setState({ unitInput: this.state.unitInput + value });
+  }
+};
+
+handleUnitCalculation = () => {
+  const units = parseFloat(this.state.unitInput);
+  if (isNaN(units) || units < 0) {
+    this.setState({ calculatedBill: "Invalid Input" });
+    return;
+  }
+
+  let amount = 0;
+
+
+  if (units <= 100) amount = 0;
+  else if (units <= 200) amount = (units - 100) * 2.25;
+  else if (units <= 300) amount = 100 * 2.25 + (units - 200) * 4.5;
+  else amount = 100 * 2.25 + 100 * 4.5 + (units - 300) * 6;
+
+  this.setState({ calculatedBill: `₹${amount.toFixed(2)}` });
+};
+  render() {
+    // const { showModal } = this.state;
+
+    return (
+      <div className="App">
+        <ToastContainer />
+
+
+<div className="unit-back">
+  
+        {/* unit calculator */}
+        <div className="eb-bill-calculator">
+  <h2 className="calc-heading"> UNIT EB Bill Calculator</h2>
+  {/* <p className="calc-subtitle">(Simple Unit-Based Bill Estimation)</p> */}
+
+  <div className="calc-display-box">
+    <input type="text" value={this.state.unitInput} readOnly className="calc-display" />
+  </div>
+
+  <div className="keypad">
+    {["1","2","3","4","5","6","7","8","9",".","0","C"].map((btn, index) => (
+      <button key={index} onClick={() => this.handleKeypad(btn)} className="key-btn">
+        {btn}
+      </button>
+    ))}
+  </div>
+
+  <button className="final-calc-btn" onClick={this.handleUnitCalculation}>
+     Calculate Bill
+  </button>
+
+  {this.state.calculatedBill !== null && (
+    // <p className="">
+    //   🧾 Your EB Bill is: <strong>{this.state.calculatedBill}</strong>
+    // </p>
+    <div
+    style={{
+      marginTop: "5px",
+      marginBottom: "-15px",
+      color: "#fff",
+      fontSize: "19px",
+      textAlign: "center",
+      fontWeight: "600",
+      animation: "slideUp 0.6s ease-out",
+      backgroundColor:"transparent"
+    }}
+  >
+     Your EB Bill is: <span style={{ color: "#FFD700" }}>{this.state.calculatedBill}</span>
+  </div>
+  )}
+</div>
+</div>
+
+      </div>
+    );
+    
+  }
+}
+
+export default App;
---
+++ src/OrderConfirmPage.js
@@ -0,0 +1,29 @@
+// // src/OrderConfirmPage.js
+
+
+// function OrderConfirmPage() {
+//   const navigate = useNavigate();
+
+//   const handleYes = () => {
+//     alert("✅ Order Placed!");
+//     navigate("/"); // Go back to home page
+//   };
+
+//   const handleNo = () => {
+//     alert("❌ Order Cancelled.");
+//     navigate("/"); // Go back to home or stay
+//   };
+
+//   return (
+//     <div className="order">
+//       <p>Did you want to place the order?</p>
+   
+//       <div className="btn-group">
+//         <button onClick={handleYes}>Yes</button>
+//         <button onClick={handleNo}>No</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default OrderConfirmPage;
